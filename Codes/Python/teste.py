72c3925485e31aaf49c52a7834fc88fe72afc696
Andre Trovello <andre.a.trovello@gmail.com>
2025-05-14 18:41:02 -0300

    cubo de dados 2

--- Codes/Python/teste.py
+++ Codes/Python/teste.py
@@ -4,6 +4,41 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import numpy as np
 
+
+
+def parse_fits_header(filename):
+    with fits.open(filename) as hdul:
+        hdr = hdul[0].header
+        
+        # Filtra apenas keywords padrão (remove HISTORY e comentários)
+        main_keys = [k for k in hdr.keys() if not k.startswith(('HISTORY', 'COMMENT', 'D00'))]
+        
+        # Cria dicionário com os metadados principais
+        metadata = {}
+        for k in main_keys:
+            try:
+                metadata[k] = hdr[k]
+            except:
+                metadata[k] = None
+        
+        # Extrai informações específicas do drizzle (se existirem)
+        drizzle_params = {}
+        for i in range(1, 10):  # Verifica até D009
+            key_prefix = f'D{i:03d}'
+            if key_prefix + 'VER' in hdr:
+                drizzle_params[f'DRIZZLE_{i}'] = {
+                    'version': hdr.get(key_prefix + 'VER'),
+                    'input': hdr.get(key_prefix + 'DATA'),
+                    'wavelength': hdr.get(key_prefix + 'LAM')
+                }
+        
+        return {
+            'main_metadata': metadata,
+            'drizzle_info': drizzle_params,
+            'history': [h for h in hdr['HISTORY'] if h.strip()]  # Lista de históricos não vazios
+        }
+
+
 #loc = os.path.expanduser("~/Desktop/Capivara_mestrado/Input/S4G")
 loc = os.path.expanduser("~/Desktop/Capivara_mestrado/Input/PHANGS/phangs_hst/ngc1087")
 # Change to the target directory
@@ -15,26 +50,24 @@ file_list = os.listdir()
 #with fits.open('NGC1087.phot.1.fits') as hdul:
 
 #print(file_list)
-print(len(file_list))
+#print(len(file_list))
+
 with fits.open('hlsp_phangs-hst_hst_wfc3-uvis_ngc1087_f275w_v1_err-drc-wht.fits') as hdul:
-    # Print the names of the HDUs in the file
     data = hdul[0].data
-    header = hdul[0].header
+    head = hdul[0].header
 
-    #print(hdul.info())
+'''phangs = parse_fits_header('hlsp_phangs-hst_hst_wfc3-uvis_ngc1087_f275w_v1_err-drc-wht.fits')
+#print(phangs['main_metadata'])
+print(data.shape)'''
 
-#print(data)
+data_cube = np.zeros((data.shape[0], data.shape[1], len(file_list)))
 
-plt.figure(figsize=(10, 10))
 for i,name in enumerate(file_list):
-    if name.endswith('.fits'):
-        with fits.open(name) as hdul:
-            # Print the names of the HDUs in the file
-            data = hdul[0].data
-            header = hdul[0].header
+    with fits.open(name) as hdul:
+        data = hdul[0].data
+        head = hdul[0].header
+        print(i, name)
+        data_cube[:,:,i] = data
 
-    plt.subplot(5,3,i+1)
-    plt.imshow(np.nan_to_num(data, nan=0), origin='lower', cmap='inferno')
-    plt.title(f'{name}')
-    plt.colorbar()
-plt.show()
+
+print(data_cube.shape)
